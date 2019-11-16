366fc762dd149fb39986935cc54777c9da82798c
Fernando Paniagua <fernando.paniagua.formacion@gmail.com>
2019-11-16 08:31:40 +0100

    Initial commit

---
+++ Assets/_GameObjects/Scripts/CameraMover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraMover : MonoBehaviour
+{
+    public Text text;
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = Camera.main.transform.eulerAngles.x.ToString();
+        if (Camera.main.transform.eulerAngles.x < 360 && Camera.main.transform.eulerAngles.x > 180)
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+    }
+}
